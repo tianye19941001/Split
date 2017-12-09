ab8a726e7715c5d292c636797cedf2f99a8d464a
tianye <838199892@qq.com>
2017-12-09 10:30:18 +0800

    first commit

---
+++ src/js/split.js
@@ -0,0 +1,9 @@
+
+function SplitJS(argument) {
+	this.type 	 = argument.type;
+	this.element = document.getElementById(this.idName);
+}
+
+SplitJS.prototype.method_name = function(first_argument) {
+	// body...
+};
